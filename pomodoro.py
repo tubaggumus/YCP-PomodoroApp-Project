457eb2493c4b4ca1a65fa6a2b9ad55e39e839853
tubaggumus <tubaggumus@gmail.com>
2024-08-14 22:35:37 +0300

    buton

--- pomodoro.py
+++ pomodoro.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 import time
-from datatime import date
+
 
 class PomodoroUyg:
     def __init__(self,master):
@@ -13,4 +13,26 @@ class PomodoroUyg:
         self.calisiyor= False
         self.kalanZaman= self.calismaZamani
         self.oturum={}
+
+        #grafik arayüz kısmı
+        self.label=tk.Label(master, text="25.00", font=("Times New Roman",48 ), foreground="purple", background="pink")
+        self.label.pack(pady=20)
+        
+        self.baslatButonu= tk.Button(master, text="Pomodoroyu Başlat", foreground="purple", command=self.zamanıBaslat)
+        self.baslatButonu.pack(pady=10)
+
+        self.durdurButonu=tk.Button(master, text="Durdur", foreground="purple", command=self.zamanıDurdur)
+        self.durdurButonu.pack(pady=10)
+
+        self.raporButonu= tk.Button(master, text="Geçmiş Raporu", command=self.raporuGoster)
+        self.raporButonu.pack(pady=10)
+    
+    def zamanıBaslat(self):
+        self.calisiyor=True
+        self.baslatButonu.config(state=tk.DISABLED)
+        self.durdurButonu.config(state=tk.NORMAL)
+        self.sayac()
+    
+    
+        
         
