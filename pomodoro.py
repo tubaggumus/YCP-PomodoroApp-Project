31a6e318f545f480aaea6028de04cb5ac34770b2
tubaggumus <tubaggumus@gmail.com>
2024-08-14 22:19:42 +0300

    tanımlama

--- pomodoro.py
+++ pomodoro.py
@@ -1,4 +1,16 @@
 import tkinter as tk
 from tkinter import messagebox
 import time
-from datatime import data
+from datatime import date
+
+class PomodoroUyg:
+    def __init__(self,master):
+        self.master=master
+        self.master.title("Pomodoro Uygulaması")
+
+        self.calismaZamani=26*60
+        self.molaZamani=5*60
+        self.calisiyor= False
+        self.kalanZaman= self.calismaZamani
+        self.oturum={}
+        
